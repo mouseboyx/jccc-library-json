f726e74207bf06f5348b4307bda62e3dd7ad5644
mouseboyx <mouseboyx@gmail.com>
2015-09-18 17:15:38 -0500

    update meta

---
+++ update.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        JCCC Library JSON
+// @namespace   JCCC Library JSON
+// @description Using https://library.jccc.edu/home/?newlibrary this script interacts with http://jccc.summon.serialssolutions.com/api/search to get all results as quickly as possible for a particular query. It can handle 1000 results total, because that is where the api cuts it off.
+// @include     about:blank?jccclibrary
+// @include     chrome://blank/?library
+// @include     https://library.jccc.edu/home/?newlibrary
+// @match       http://jccc.summon.serialssolutions.com/api/*
+// @version     0.1
+// @grant       GM_xmlhttpRequest
+// @updateURL   https://github.com/mouseboyx/jccc-library-json/blob/master/update.meta.js
+// @downloadURL https://github.com/mouseboyx/jccc-library-json/blob/master/JCCC%20Library%20JSON.user.js
+// ==/UserScript==
